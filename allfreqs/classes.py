fe5bcb257a269633570ec8c19db8e08e0f79d5ea
Roberto Preste <roberto.preste@gmail.com>
2019-08-07 19:55:25 +0100

    Refactoring to handle fasta and csv input files

--- allfreqs/classes.py
+++ allfreqs/classes.py
@@ -3,45 +3,75 @@
 # Created by Roberto Preste
 from skbio import TabularMSA, DNA, Sequence
 import pandas as pd
+# import modin.pandas as pd
 
 
 class MultiAlignment:
-    """Class that will store a multialignment.
 
-    It will read in a multisequence fasta file, then convert it to a dataframe
+    def __init__(self):
+        self.msa = None
+        self.ref = None
+
+    def from_fasta(self, input_file, with_ref=False):
+        self.msa = TabularMSA.read(input_file, constructor=DNA)
+        if with_ref:
+            self.ref = self.msa[0]
+            self.msa = self.msa[1:]
+
+    def from_csv(self, input_file, with_ref=False, **kwargs):
+        self.msa = pd.read_csv(input_file, **kwargs)
+        if with_ref:
+            self.ref = self.msa.iloc[0, :]
+            self.msa = self.msa.iloc[1:, :]
+
+
+
+
+class Multialignment:
+    """Class to read and process a multialignment.
+
+    It reads in a multisequence fasta file, then converts it to a dataframe
     with (id, sequence) columns.
     """
-    def __init__(self, input_msa):
-        self.msa = input_msa
+
+    def __init__(self, input_file):
+        self.msa = input_file
         self.tabmsa = self.read()
-        # self.df = None
 
     def read(self):
-        """Read a multifasta."""
+        """Read a multifasta using the TabularMSA class from skbio."""
         return TabularMSA.read(self.msa, constructor=DNA)
 
     @property
     def df(self):
-        """Dump the multialignment to a dataframe with (id, sequence)
+        """Convert the multialignment to a dataframe with (id, sequence)
         columns."""
         df_dict = {seq.metadata.get("id"): str(seq) for seq in self.tabmsa}
         df = pd.DataFrame.from_dict(df_dict,
                                     orient="index", columns=["sequence"])
         df.reset_index(inplace=True)
         df.rename({"index": "id"}, axis=1, inplace=True)
-        # self.df = df
         return df
 
-    def write(self):
-        self.df.to_csv("seq_df.csv", index=False)
+    def write(self, output_file: str = "sequences_df.csv"):
+        """Write the resulting dataframe to disk.
+
+        Args:
+            output_file: output file name
+        """
+        self.df.to_csv(output_file, index=False)
 
 
 class Reference:
-    """Reference genome, will be used to create the reference positions."""
+    """Class to read and process a reference genome.
+
+    It reads a reference genome in fasta format, which is used to create
+    the reference positions.
+    """
+
     def __init__(self, input_file):
         self.input_file = input_file
         self.sequence = self.read()
-        # self.indexes = None
 
     def read(self):
         """Read the input reference sequence."""
@@ -66,28 +96,35 @@ class Reference:
             else:  # gap position
                 i += 1
             indexes.append("{}.{}_{}".format(n, i, nt))
-        # self.indexes = indexes
         return indexes
 
 
-class AlignmentDataframe:
-    """Input: Reference(), Multialignment().
-    Output: dataframe with allele frequencies."""
+class AlleleFreqs:
+    """Class used to calculate allele frequencies from a multialignment.
+
+    Input: Reference(), Multialignment().
+    Output: dataframe with allele frequencies.
+    """
+
     def __init__(self, reference, sequences):
         self.reference = Reference(reference)
-        self.sequences = MultiAlignment(sequences)
-        self.df = None
-        self.var_df = None
+        self.multialg = Multialignment(sequences)
 
-    def to_df(self):
-        df = self.sequences.df["sequence"].str.split("", expand=True)
+    @property
+    def df(self):
+        """Convert sequences to the proper dataframe for further allele
+        frequency calculations."""
+        df = self.multialg.df["sequence"].str.split("", expand=True)
         df.drop([0, df.columns[len(df.columns) - 1]], axis=1, inplace=True)
         df.fillna("-", inplace=True)  # avoidable
-        df.index = self.sequences.df["id"]
+        df.index = self.multialg.df["id"]
         df.columns = self.reference.indexes
-        self.df = df
+        return df
 
-    def calculate(self):
+    @property
+    def frequencies(self):
+        """Calculate allele frequencies for the 4 basic nucleotides,
+        gaps and other (non-canonical) nucleotides."""
         var_df = pd.DataFrame(columns=["A", "C", "G", "T", "gap", "oth"])
         for col in self.df.columns:
             freqs = self.df[col].value_counts(normalize=True)
@@ -104,4 +141,12 @@ class AlignmentDataframe:
                                              index=[col])])
         var_df.reset_index(inplace=True)
         var_df.rename({"index": "position"}, axis=1, inplace=True)
-        self.var_df = var_df
+        return var_df
+
+    def write(self, output_file: str = "all_freqs.csv"):
+        """Write the resulting allele frequency dataframe to disk.
+
+        Args:
+            output_file: output file name
+        """
+        self.frequencies.to_csv(output_file, index=False)
