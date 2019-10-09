ee4118b73b02bcc6509035035f1b658fa4024550
Roberto Preste <roberto.preste@gmail.com>
2019-10-09 12:36:04 +0200

    Add tests with reference included in multialignment

--- tests/test_allfreqs.py
+++ tests/test_allfreqs.py
@@ -50,9 +50,41 @@ class TestAlleleFreqsFasta:
         assert_frame_equal(result, sample_frequencies_csv)
 
 
+class TestAlleleFreqsWithRefFasta:
+    a = AlleleFreqs.from_fasta(sequences=os.path.join(DATADIR,
+                                                      "sample_multialg_with_ref.fasta"))
+
+    def test_df(self, sample_sequences_df):
+        assert_frame_equal(self.a.df, sample_sequences_df)
+
+    def test_freqs(self, sample_sequences_freqs):
+        assert_frame_equal(self.a.frequencies, sample_sequences_freqs)
+
+    def test_to_csv(self, sample_frequencies_csv):
+        self.a.to_csv(TEST_CSV)
+        result = pd.read_csv(TEST_CSV)
+        assert_frame_equal(result, sample_frequencies_csv)
+
+
 class TestAlleleFreqsCsv:
     a = AlleleFreqs.from_csv(sequences=os.path.join(DATADIR, "sample_multialg.csv"),
                              reference=os.path.join(DATADIR, "sample_ref.csv"))
+
+    def test_df(self, sample_sequences_df):
+        assert_frame_equal(self.a.df, sample_sequences_df)
+
+    def test_freqs(self, sample_sequences_freqs):
+        assert_frame_equal(self.a.frequencies, sample_sequences_freqs)
+
+    def test_to_csv(self, sample_frequencies_csv):
+        self.a.to_csv(TEST_CSV)
+        result = pd.read_csv(TEST_CSV)
+        assert_frame_equal(result, sample_frequencies_csv)
+
+
+class TestAlleleFreqsWithRefCsv:
+    a = AlleleFreqs.from_csv(sequences=os.path.join(DATADIR,
+                                                    "sample_multialg_with_ref.csv"))
 
     def test_df(self, sample_sequences_df):
         assert_frame_equal(self.a.df, sample_sequences_df)
