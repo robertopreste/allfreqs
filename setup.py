45cc87aff54e4800810efe9d1c59bacf84e98212
Roberto Preste <roberto.preste@gmail.com>
2019-10-17 18:47:37 +0100

    Fix requirements and testing framework

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(  # pragma: no cover
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
