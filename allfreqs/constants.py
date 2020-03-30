c3a169453562ad6feab32f3ca3a17fea82fc7f56
Roberto Preste <roberto.preste@gmail.com>
2020-03-30 22:25:08 +0100

    ENH: Add option to allow for ambiguous nucleotides

---
+++ allfreqs/constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Roberto Preste
+
+STANDARD_COLS = ["A", "C", "G", "T", "gap", "oth"]
+
+AMBIGUOUS_COLS = ["A", "C", "G", "T", "R", "Y", "K", "M", "S", "W", "B", "D",
+                  "H", "V", "N", "gap"]
